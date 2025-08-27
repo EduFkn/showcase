f1e2079cd2d022d33b38b75517051645802f3a8c
EduFkn <ploutzin129@gmail.com>
2025-08-26 21:59:26 -0300

    feat: add initial hero section, theme providers and clsx components ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-import Image from "next/image";
+import Hero from "@/components/Hero";
+
 
 export default function Home() {
   return (
-    <main>
-      <div>
-        <h1>Hello world</h1>
+    <main style={{
+      background: "#000319"
+    }} className="relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <Hero />
       </div>
     </main>
   );
